c334485050f3ce6967d76143102039a290eac819
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-12 19:58:38 +0100

    check user-number with random number, add message with right number and point

--- js/script.js
+++ js/script.js
@@ -10,15 +10,16 @@ const result = document.getElementById('result')
 button.addEventListener('click', () => {
     button.classList.add('hidden')
     let numbers = []
+
     //Genero 5 numeri casuali da 1 a 100
     for (let i = 0; i < 5; i++) {
-        const number = Math.floor(Math.random() * 100) + 1
+        const number = parseInt(Math.floor(Math.random() * 100) + 1)
         numbers.push(number)
     }
     //Stampo i numeri casuali in pagina
     numbersPage.innerText = numbers.join(' - ')
     //Facci partire il timer da 30
-    let timer = 3
+    let timer = 30
     //Stampo il timer in pagina
     timerPage.innerText = `TIMER: ${timer}`
     //Diminuisco ogni secondo in valore di timer
@@ -26,16 +27,28 @@ button.addEventListener('click', () => {
         --timer
         timerPage.innerText = `TIMER: ${timer}`
         if (timer === 0) {
+            //Interrompo l'intervallo
             clearInterval(countDown)
+            //Aggiungo e rimuovo eventuali classi
             numbersPage.classList.add('hidden')
             timerPage.classList.add('hidden')
             button.classList.remove('hidden')
-            for (let i = 0; i < 5; i++) {
-                const numberUser = parseInt(prompt('Inserisci i numeri visti in precedenza'))
+            //Chiado all'utente tanti numeri quanti i numeri randomici 
+            let numberUser = []
+            for (let i = 0; i < numbers.length; i++) {
+                numberUser.push(parseInt(prompt('Inserisci i numeri visti in precedenza')))
+                console.log(numberUser)
+                console.log(numbers)
+                //Se il numero dell'utente è uguale al numero randomico generato in pagina allora lo inserisco nel risultato.
+                if (numberUser[i] === numbers[i]) {
+                    //Creo il messaggio finale con numeri indovinati e punteggio
+                    result.innerText = `Hai indovinato questi numeri:${numberUser} per un totale di ${numberUser.length} punti! `
+                }
             }
             button.innerText = 'Riprova'
         }
         else {
+            //Rimuobo eventuali classi
             numbersPage.classList.remove('hidden')
             timerPage.classList.remove('hidden')
             button.classList.remove('remove')
