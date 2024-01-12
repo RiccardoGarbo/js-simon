2feb4ea365f7c3e5b7492b06109ebb7326873448
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-12 17:15:30 +0100

    create variables ''element html''

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Trasferisco gli elementi della pagina
+
+const button = document.getElementById('button')
+const timer = document.getElementById('timer')
+const result = document.getElementById('result')
