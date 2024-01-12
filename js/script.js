85e85628d1aa1ae7ce30ecb8d86d1ffb47515a12
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-12 17:17:57 +0100

    add element html, and variable js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 //Trasferisco gli elementi della pagina
 
 const button = document.getElementById('button')
+const numbersPage = document.getElementById('numbers')
 const timer = document.getElementById('timer')
 const result = document.getElementById('result')
+
+//Al click del bottone faccio apparire in pagina 
