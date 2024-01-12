f773f3fcdaf2385d7d8e5a556fd81f42a2b20915
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-12 18:15:15 +0100

    generates 5 random numbers and start 30sec cd

--- js/script.js
+++ js/script.js
@@ -2,7 +2,30 @@
 
 const button = document.getElementById('button')
 const numbersPage = document.getElementById('numbers')
-const timer = document.getElementById('timer')
+const timerPage = document.getElementById('timer')
 const result = document.getElementById('result')
 
 //Al click del bottone faccio apparire in pagina 
+
+button.addEventListener('click', () => {
+    let numbers = []
+    //Genero 5 numeri casuali da 1 a 100
+    for (let i = 0; i < 5; i++) {
+        const number = Math.floor(Math.random() * 100) + 1
+        numbers.push(number)
+    }
+    //Stampo i numeri casuali in pagina
+    numbersPage.innerText = numbers.join(' - ')
+    //Facci partire il timer da 30
+    let timer = 5
+    //Stampo il timer in pagina
+    timerPage.innerText = `TIMER: ${timer}`
+    //Diminuisco ogni secondo in valore di timer
+    const countDown = setInterval(function () {
+        --timer
+        timerPage.innerText = `TIMER: ${timer}`
+
+    }, 1000)
+
+}
+)
