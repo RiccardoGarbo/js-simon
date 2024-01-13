b6669808a88b5af07bf5a322f1fe08438b1948d9
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-13 14:54:06 +0100

    now the numbers page are different random number

--- js/script.js
+++ js/script.js
@@ -9,17 +9,21 @@ const result = document.getElementById('result')
 
 button.addEventListener('click', () => {
     button.classList.add('hidden')
+    result.innerText = ' '
     let numbers = []
 
     //Genero 5 numeri casuali da 1 a 100
-    for (let i = 0; i < 5; i++) {
-        const number = parseInt(Math.floor(Math.random() * 100) + 1)
-        numbers.push(number)
+    while (numbers.length < 5) {
+        let number = parseInt(Math.floor(Math.random() * 6) + 1)
+        if (!numbers.includes(number))
+            numbers.push(number)
+        console.log(numbers)
     }
     //Stampo i numeri casuali in pagina
     numbersPage.innerText = numbers.join(' - ')
     //Facci partire il timer da 30
-    let timer = 30
+    let timer = 3
+
     //Stampo il timer in pagina
     timerPage.innerText = `TIMER: ${timer}`
     //Diminuisco ogni secondo in valore di timer
@@ -31,19 +35,24 @@ button.addEventListener('click', () => {
             clearInterval(countDown)
             //Aggiungo e rimuovo eventuali classi
             numbersPage.classList.add('hidden')
-            timerPage.classList.add('hidden')
             button.classList.remove('hidden')
+            timerPage.classList.add('hidden')
             //Chiado all'utente tanti numeri quanti i numeri randomici 
             let numberUser = []
+
+
             for (let i = 0; i < numbers.length; i++) {
+
                 numberUser.push(parseInt(prompt('Inserisci i numeri visti in precedenza')))
-                console.log(numberUser)
-                console.log(numbers)
-                //Se il numero dell'utente è uguale al numero randomico generato in pagina allora lo inserisco nel risultato.
                 if (numberUser[i] === numbers[i]) {
                     //Creo il messaggio finale con numeri indovinati e punteggio
                     result.innerText = `Hai indovinato questi numeri:${numberUser} per un totale di ${numberUser.length} punti! `
                 }
+                console.log(numberUser)
+                console.log(numbers)
+
+
+                //Se il numero dell'utente è uguale al numero randomico generato in pagina allora lo inserisco nel risultato.
             }
             button.innerText = 'Riprova'
         }
